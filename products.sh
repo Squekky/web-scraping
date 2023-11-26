f1de518f98e4c1e5b479e42dbc1430b79b88b448
Ryan <34011450+Squekky@users.noreply.github.com>
2023-11-26 12:37:28 -0500

    Update products.sh
    Now downloads TagSoup if it isn't downloaded

--- products.sh
+++ products.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 1 ];
 then
     echo Invalid parameters, requires two text files
     exit 1
+fi
+
+# Download tagsoup if not in directory
+if ! [ -f tagsoup-1.2.1.jar ];
+then
+    wget https://repo1.maven.org/maven2/org/ccil/cowan/tagsoup/tagsoup/1.2.1/tagsoup-1.2.1.jar
 fi
 
 while true
@@ -11,23 +17,25 @@ do
     echo Running
     for vendor in $1 $2
     do
+        count=1
         vendor=${vendor::-4}
         mkdir -p $vendor
-        echo Made Directories
-        count=1
+        echo Made directory
         for item in $(cat $vendor.txt)
         do
             echo Downloading $item
             curl -s $item > $vendor/$count.html
+
             echo TagSouping
             java -jar tagsoup-1.2.1.jar --files $vendor/$count.html
-            echo Deleting original HTML file
-            rm $vendor/$count.html
+
             echo Getting info from file
             python3 parser.py $vendor/$count.xhtml $vendor
+
             count=`expr $count + 1`
         done
         rm -r $vendor
+        echo Removed directory
     done
     sleep 21600
     echo ""
